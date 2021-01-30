265c388c03775c64f4e35af90688d355ce3146c7
ferron gerald <opstyle1@hotmail.fr>
2021-01-30 11:26:53 +0100

    back securiser

--- Backend/routes/form.js
+++ Backend/routes/form.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+const verifyEmail = require("../middleware/email");
+
 const formulaire = require("../controllers/poster");
 
-router.post("/poster", formulaire.post);
+router.post("/poster", verifyEmail, formulaire.post);
 router.get("/client", formulaire.getAll);
 
 module.exports = router;
